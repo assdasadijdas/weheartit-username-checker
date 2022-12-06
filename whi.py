b7f24d4ef2f2aa757c03cfe33c8da88756a5ce26
_ <117321203+littleboybigworld@users.noreply.github.com>
2022-12-06 23:41:20 +0000

    Update whi.py
    better

--- whi.py
+++ whi.py
@@ -2,19 +2,17 @@ import requests
 import numpy as np
 from multiprocessing import Pool
 
-case1 = "Username is associated with a verified twitter account"
-case2 = "Username has already been taken"
-
 def plugrun(i):
     output = open(f"available.txt", "a+", encoding="utf-8")
     resp = requests.get(f"https://weheartit.com/users/validate?user%5Busername%5D={i}")
-    boob = str(resp.json())
-    if (boob.find(case1) == -1) and (boob.find(case2) == -1):
-        print(f"{i} is available")
+    try:
+        resp.json()["errors"]["username"]
+        print(f"{i} not available")
+    except:
         output.write(f"{i}\n")
-    else:
-        print(f"{i} isnt available")
+        print(f"{i} available")
 
+    
 def main(threads, array):
     with Pool(threads) as p:
         p.map(plugrun, array)
